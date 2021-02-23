b14e1a4b75eabac4ebdde17dfdbce5697ccb8837
Morgan Collado <morgan.collado@gmail.com>
2021-02-23 17:49:40 -0600

    adjusts cors, adds mock log in for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+    
+        def current_user
+            #mock login
+            User.first
+        end 
+
+        def logged_in?
+            !!current_user
+        end 
 end
