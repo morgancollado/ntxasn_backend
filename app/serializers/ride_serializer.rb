adf436866c4e98d48e775887b9e3e2c8470ce49c
Morgan Collado <morgan.collado@gmail.com>
2021-02-27 18:18:59 -0600

    customizes serialized data to send info about the driver and passenger for rides

--- app/serializers/ride_serializer.rb
+++ app/serializers/ride_serializer.rb
@@ -1,4 +1,4 @@
 class RideSerializer
   include JSONAPI::Serializer
-  attributes :date_time, :location_to, :location_from, :appointment_type, :passenger_id, :driver_id
+  attributes :date_time, :location_to, :location_from, :appointment_type, :passenger, :driver
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,10 @@
 class UserSerializer
   include JSONAPI::Serializer
   attributes :name, :email, :phone_number, :passenger_rides, :driver_rides
+  attributes :passenger_rides do |user|
+    RideSerializer.new(user.passenger_rides).as_json['data']
+  end 
+  attributes :driver_rides do |user|
+    RideSerializer.new(user.driver_rides).as_json['data']
+  end
 end
