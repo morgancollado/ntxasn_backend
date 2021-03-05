533a174795ce172fc959b8390f6587a5efa3ebf2
Morgan Collado <morgan.collado@gmail.com>
2021-03-05 08:56:26 -0600

    adds strftime method to rides in order to format date_time in the backend

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,8 @@ class Ride < ApplicationRecord
     belongs_to :passenger, :class_name => "User"
     belongs_to :driver, :class_name => "User", optional: true 
     validates_presence_of :date_time, :location_to, :location_from, :appointment_type
+
+    def strftime
+        self.date_time.strftime("%A, %d %b %Y %l:%M %p")
+    end 
 end
--- app/serializers/ride_serializer.rb
+++ app/serializers/ride_serializer.rb
@@ -1,4 +1,7 @@
 class RideSerializer
   include JSONAPI::Serializer
   attributes :date_time, :location_to, :location_from, :appointment_type, :passenger, :driver
+  attributes :date_time do |date_time|
+    date_time.strftime
+  end 
 end
