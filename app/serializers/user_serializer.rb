ea5d1472e08315ac233dcf7ebbf5f13f9590e605
morgancollado <morgan.collado@gmail.com>
2021-03-19 18:21:26 -0500

    adds days of the week to seralizer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :name, :email, :phone_number,:passenger_rides, :driver_rides, :availability_hours_lower, :availability_hours_upper
+  attributes :name, :email, :phone_number,:passenger_rides, :driver_rides, :availability_hours_lower, :availability_hours_upper, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday
   attributes :passenger_rides do |user|
     RideSerializer.new(user.passenger_rides).as_json['data']
   end 
