30ab0090efd97d4743d9c8b35d6a276f36e65ce3
morgancollado <morgan.collado@gmail.com>
2021-03-18 23:16:37 -0500

    adds availability_hours_upper, availability_hours_lower and availability_days to user seralizer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :name, :email, :phone_number, :passenger_rides, :driver_rides
+  attributes :name, :email, :phone_number,:passenger_rides, :driver_rides, :availability_days, :availability_hours_lower, :availability_hours_upper
   attributes :passenger_rides do |user|
     RideSerializer.new(user.passenger_rides).as_json['data']
   end 
