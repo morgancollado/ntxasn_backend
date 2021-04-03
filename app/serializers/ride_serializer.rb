b255edfe048c27704b24168f8e945542df05b881
morgancollado <morgan.collado@gmail.com>
2021-04-03 13:08:47 -0500

    adds stage attribute to seralizer

--- app/serializers/ride_serializer.rb
+++ app/serializers/ride_serializer.rb
@@ -1,6 +1,6 @@
 class RideSerializer
   include JSONAPI::Serializer
-  attributes :date_time, :location_to, :location_from, :appointment_type, :passenger, :driver, :initial_pickup, :clinic_dropoff, :clinic_pickup, :final_dropoff
+  attributes :date_time, :location_to, :location_from, :appointment_type, :passenger, :driver, :initial_pickup, :clinic_dropoff, :clinic_pickup, :final_dropoff, :stage
   attributes :date_time do |date_time|
     date_time.strftime
   end 
